c234c1e340e03fdd66274fe7864c0e1c0250ece7
Zachary Mowatt <zamowatt@gmail.com>
2025-01-25 17:44:32 -0500

    completed lab

--- index.js
+++ index.js
@@ -2,10 +2,55 @@ function addingEventListener() {
     const input = document.getElementById('button');
     
     function clickAlert(){
-      alert('He-He! That Tickles!')
+      alert('I\'m Batman!')
     }
 
     input.addEventListener('click', clickAlert);
 }
 
 addingEventListener();
+
+
+
+
+
+
+
+  const input = document.getElementById('button')
+
+  input.addEventListener('click', function(){
+    alert('I\'m Manbat!!')
+  })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
